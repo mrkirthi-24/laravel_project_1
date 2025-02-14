0693afe181d5f50c3a1a12fa56882bd8efd62a8c
mrkirthi-24 <kirthibagrecha4@gmail.com>
2025-02-14 23:15:53 +0530

    setup db with seed data

---
+++ database/seeders/UsersTablesseeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTablesseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'John Doe',
+                'email' => 'john@example.com',
+                'password' => bcrypt('password'),
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
